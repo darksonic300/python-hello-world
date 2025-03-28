af9f6591428003a426aef6a297565ca08ae2d51d
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-28 22:58:12 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,6 +10,6 @@ class handler(BaseHTTPRequestHandler):
 
         string = '{"platform": "Instagram", "time": 20, "data": 200}, {"platform": "Facebook", "time": 30, "data": 300}, {"platform": "Twitter", "time": 40, "data": 400 }, {"platform": "LinkedIn", "time": 50, "data": 500}, {"platform": "TikTok", "time": 60, "data": 600}'
         
-        #self.wfile.write(json.loads(string, sort_keys=True, indent=4)))
-        self.wfile.write(string)
+        self.wfile.write(json.dumps(string, sort_keys=True, indent=4)))
+        #self.wfile.write(string)
         return
