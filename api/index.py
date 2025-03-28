9438089798d6672b08d593ac4cf41eccfd2f8033
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-28 23:15:55 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,6 +9,16 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         string = '{"platform": "Instagram", "time": 20, "data": 200}, {"platform": "Facebook", "time": 30, "data": 300}, {"platform": "Twitter", "time": 40, "data": 400 }, {"platform": "LinkedIn", "time": 50, "data": 500}, {"platform": "TikTok", "time": 60, "data": 600}'
+
+        data = [
+            {"platform": "Instagram", "time": 20, "data": 200},
+            {"platform": "Facebook", "time": 30, "data": 300},
+            {"platform": "Twitter", "time": 40, "data": 400},
+            {"platform": "LinkedIn", "time": 50, "data": 500},
+            {"platform": "TikTok", "time": 60, "data": 600}
+        ]
+
         
-        self.wfile.write(string)
+        
+        self.wfile.write(json.dumps(data))
         return
