999dcec3ab8490c53dddaf83e1dd8632cc0b1200
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-28 22:30:44 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,32 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','application/json')
         self.end_headers()
 
-        string = """
-        {
-          "platform": "Instagram",
-          "time": 20,
-          "data": 200
-        },
-        {
-          "platform": "Facebook",
-          "time": 30,
-          "data": 300
-        },
-        {
-          "platform": "Twitter",
-          "time": 40,
-          "data": 400
-        }
-        {
-          "platform": "LinkedIn",
-          "time": 50,
-          "data": 500
-        },
-        {
-          "platform": "TikTok",
-          "time": 60,
-          "data": 600
-        }"""
+        string = "{"platform": "Instagram", "time": 20, "data": 200}, {"platform": "Facebook", "time": 30, "data": 300}, {"platform": "Twitter", "time": 40, "data": 400 }, {"platform": "LinkedIn", "time": 50, "data": 500}, {"platform": "TikTok", "time": 60, "data": 600}"
         
-        self.wfile.write(string.encode('utf-8'))
+        self.wfile.write(json.loads(string))
         return
