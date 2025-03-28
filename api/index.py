08e9cbfb2e2a710bcd60f1e46b8cf63fa2456e2b
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-28 22:23:48 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,36 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','application/json')
         self.end_headers()
+
+        string = """
+        {
+          "platform": "Instagram",
+          "time": 20,
+          "data": 200
+        },
+        {
+          "platform": "Facebook",
+          "time": 30,
+          "data": 300
+        },
+        {
+          "platform": "Twitter",
+          "time": 40,
+          "data": 400
+        }
+        {
+          "platform": "LinkedIn",
+          "time": 50,
+          "data": 500
+        },
+        {
+          "platform": "TikTok",
+          "time": 60,
+          "data": 600
+        }
+        """
+        
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
