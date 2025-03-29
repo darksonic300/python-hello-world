c801e5641370f06271b0753beaa3a30ba00429c8
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-29 04:51:24 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,8 +7,9 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')  # Allow all origins
-        self.send_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')  # Allow specific headers
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
         self.end_headers()
 
         string = '{"platform": "Instagram", "time": 20, "data": 200}, {"platform": "Facebook", "time": 30, "data": 300}, {"platform": "Twitter", "time": 40, "data": 400 }, {"platform": "LinkedIn", "time": 50, "data": 500}, {"platform": "TikTok", "time": 60, "data": 600}'
