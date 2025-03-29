46d1b28bafe2de59b26bacea0510d15025c761b1
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-03-29 06:21:13 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,7 +23,7 @@ class handler(BaseHTTPRequestHandler):
             {"platform": "Twitter", "time": random.randint(5, 120), "data": random.randint(100, 10000)},
             {"platform": "LinkedIn", "time": random.randint(5, 120), "data": random.randint(100, 10000)},
             {"platform": "TikTok", "time": random.randint(5, 120), "data": random.randint(100, 10000)},
-            {"total": 2583}
+            {"total": random.randint(1000, 50000)}
         ]
     
         self.wfile.write(json.dumps(data).encode('utf-8'))
